d933b5ea9aeeef6f8014bbedaf0cc9ae90c90138
cjada <c.james.rhoda@gmail.com>
2018-10-02 13:32:40 -0500

    added rough sudoku GUI implementation

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,3 +1,118 @@
+from tkinter import Tk, Canvas, Frame, Button, BOTH, TOP, BOTTOM
+
+class Sudoku_UI(Frame):
+	"""
+	The UI for drawing the board and accepting user input.
+	"""
+	def __init__(self, parent, game):
+		self.game = game
+		self.parent = parent
+		Frame.__init__(self, parent)
+
+		self.row, self.column = 0, 0
+
+		self.__initUI()
+
+	def __initUI(self):
+		self.parent.title("Sudoku Solver")
+		self.pack(fill=BOTH, expand=1)
+		self.canvas = Canvas(self, width=WIDTH, height=HEIGHT)
+		self.canvas.pack(fill=BOTH, side=TOP)
+		clear_button = Button(self, text="Clear answers", command=self.__clear_answers)
+		clear_button.pack(fill=BOTH, side=BOTTOM)
+
+		self.__draw_grid()
+		self.__draw_puzzle()
+
+		self.canvas.bind("<Button-1>", self.__cell_clicked)
+		self.canvas.bind("<Key>", self.__key_pressed)
+
+	def __draw_grid(self):
+		"""
+		Draw grid divided by blue lines into 3x3 squares
+		"""
+		for i in range(10):
+			color = "blue" if i % 3 == 0 else "gray"
+
+			x0 = MARGIN + i * SIDE 
+			y0 = MARGIN
+			x1 = MARGIN + i * SIDE 
+			y1 = HEIGHT - MARGIN
+			self.canvas.create_line(x0, y0, x1, y1, fill=color)
+
+			x0 = MARGIN
+			y0 = MARGIN + i * SIDE
+			x1 = WIDTH - MARGIN
+			y1 = MARGIN + i * SIDE
+			self.canvas.create_line(x0, y0, x1, y1, fill=color)
+
+	def  __draw_puzzle(self):
+		self.canvas.delete("numbers")
+		for i in range(9):
+			for j in range(9):
+				answer = self.game.puzzle[i][j]
+				if answer != 0:
+					x = MARGIN + j * SIDE + SIDE / 2
+					y = MARGIN + i * SIDE + SIDE / 2
+					original = self.game.start_puzzle[i][j]
+					color = "black" if answer == original else "sea green"
+					self.canvas.create_text(
+						x, y, text=answer, tags="numbers", fill=color
+					)
+
+	def __clear_answers(self):
+		self.game.start()
+		self.canvas.delete("victory")
+		self.__draw_puzzle()
+
+	def __cell_clicked(self, event):
+		if self.game.game_over:
+			return
+
+		x, y = event.x, event.y
+		if (MARGIN < x < WIDTH - MARGIN and MARGIN < y < HEIGHT - MARGIN):
+			self.canvas.focus_set()
+
+			row, col = (y - MARGIN) / SIDE, (x - MARGIN) / SIDE
+
+			if (row, col) = (self.row, self.col):
+				self.row, self.col = -1, -1
+			elif self.game.puzzle[row][col] == 0:
+				self.row, self.col = row, col
+
+		self.__draw_cursor()
+
+	def __draw_cursor(self):
+		self.canvas.delete("cursor")
+		if self.row >= 0 and self.col >= 0:
+			x0 = MARGIN + self.col * SIDE + 1
+			y0 = MARGIN + self.row * SIDE + 1
+			x1 = MARGIN + (self.col + 1) * SIDE - 1
+			y1 = MARGIN + (self.row + 1) * SIDE - 1
+			self.canvas.create_rectangle(
+				x0, y0, x1, y1,
+				outline = "red", tags="cursor"
+			)
+
+	def __key_pressed(self, event):
+		if self.game.gmae_over:
+			return
+		if self.row >= 0 and self.col >= and event.char in "1234567890":
+			self.game.puzzle[self.row][self.col] = int(event.char)
+			self.col, self.row = -1, -1
+			self.__draw_puzzle()
+			self.__draw_cursor()
+			"""
+			This is where VICTORY would go.  Maybe include a message
+			saying that this problem is solvable (or not)?
+			"""
+
+
+
+
+
+
+
 class Sudoku_solver(object):
 	"""docstring for Sudoku_solver"""
 	def __init__(self, arg):
